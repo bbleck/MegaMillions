89da0d47029d72739768a9cf5a15063ea9e81be0
brianb <bbleck@unm.edu>
2018-10-23 15:08:54 -0600

    Added basic database structures.

--- app/src/main/java/deepdive/cnm/edu/megamillions/view/PickAdapter.java
+++ app/src/main/java/deepdive/cnm/edu/megamillions/view/PickAdapter.java
@@ -1,8 +1,8 @@
 package deepdive.cnm.edu.megamillions.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -11,16 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import deepdive.cnm.edu.megamillions.R;
-import java.util.Arrays;
 import java.util.List;
 
 public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder>{
 
   private List<int[]> picks;
-  private AppCompatActivity mContext;
+  private Context mContext;
 
 
-  public PickAdapter(AppCompatActivity context, List<int[]> picks){
+  public PickAdapter(Context context, List<int[]> picks){
     this.picks = picks;
     this.mContext = context;
   }
@@ -28,13 +27,17 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder>{
   @NonNull
   @Override
   public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-    View view = LayoutInflater.from(mContext).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+    View view = LayoutInflater.from(mContext).inflate(R.layout.pick_item, viewGroup, false);
+
     return new Holder(view);
   }
 
   @Override
   public void onBindViewHolder(@NonNull Holder holder, int position) {
     holder.bind();
+    if(position%2 == 1){
+      holder.itemView.setBackgroundColor(Color.argb(16, 0, 0, 0));
+    }
   }
 
 
@@ -46,18 +49,26 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder>{
 
   public class Holder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
 
-    private TextView view;
+    private TextView[] numbers;
+    private static final int PICK_LENGTH = 6;
+
 
     public Holder(@NonNull View itemView) {
       super(itemView);
-      this.view = (TextView) itemView;
-
+      itemView.setOnCreateContextMenuListener(this);
+      numbers = new TextView[PICK_LENGTH];
+      for (int i = 0; i < PICK_LENGTH; i++) {
+        int id = mContext.getResources().getIdentifier("num_"+i, "id", mContext.getPackageName());
+        numbers[i] = itemView.findViewById(id);
+      }
     }
 
     private void bind(){
       int[] numbers = picks.get(getAdapterPosition());
-      view.setText(Arrays.toString(numbers));
-      view.setOnCreateContextMenuListener(this);
+      for (int i = 0; i < numbers.length; i++) {
+        this.numbers[i].setText(String.format("%02d", numbers[i]));
+      }
+
     }
 
     @Override
